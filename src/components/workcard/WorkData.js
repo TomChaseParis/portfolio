25dc8cdfa41f5b86734227fafe79d0cc992a5cfa
Thomas Chase <chasethomasparis@gmail.com>
2022-11-25 21:10:37 +0100

    add files

---
+++ src/components/workcard/WorkData.js
@@ -0,0 +1,61 @@
+import pro1 from '../../assets/deliveryfood.png'
+import pro2 from '../../assets/realestate.png'
+import pro3 from '../../assets/youtube.png'
+import pro4 from '../../assets/paradisebeach.png'
+import pro5 from '../../assets/expense.png'
+import pro6 from '../../assets/caraSITE.png'
+import pro7 from '../../assets/dashboard.png'
+
+const ProjectCardData = [
+  {
+    imgSrc: pro1,
+    title: 'Delivery Food App',
+    text: 'Food Delivery APP using React, Redux, Firebase & Material-UI..., Mimicking a deliveroo-like app, this app allows user to choose and validate his/her order',
+    view: 'https://github.com',
+    linkURL: 'https://github.com/TomChaseParis/Food-Delivery-App',
+  },
+  {
+    imgSrc: pro2,
+    title: 'Real Estate App',
+    text: 'Building & Deploying a real estate website from scratch. Using CSS GRID, CSS Order property.',
+    view: '',
+    linkURL: 'https://github.com/TomChaseParis/my-real-estate',
+  },
+  {
+    imgSrc: pro3,
+    title: 'Clone Youtube App',
+    text: 'Building & Deploying a Modern Youtbe Clone Application with Material UI 5 & RapidAPI ',
+    view: '',
+    linkURL: 'https://github.com/TomChaseParis/My-Youtube',
+  },
+  {
+    imgSrc: pro4,
+    title: 'Travel Agency App',
+    text: 'Above a real-estate agency made via react. This app was intented to provide users with an up-to-dream travel experience',
+    view: '',
+    linkURL: 'https://github.com/TomChaseParis/Travel-Agency',
+  },
+  {
+    imgSrc: pro5,
+    title: 'Expense Tracker App',
+    text: 'Project in vanilla JS expense tracker app and turn it into a React app using hooks (useState, useContext, useReducer) and the context API.',
+    view: '',
+    linkURL: 'https://github.com/TomChaseParis/Expense-Tracker',
+  },
+  {
+    imgSrc: pro6,
+    title: 'Cara Site',
+    text: 'Building & Deploy Fictional E-commerce Website with HTML, CSS & JavaScript',
+    view: '',
+    linkURL: 'https://github.com/TomChaseParis/CARA-SITE',
+  },
+  {
+    imgSrc: pro7,
+    title: 'React Admin Dashboard',
+    text: 'React Admin Dashboard from scratch. I will be using React, Material UI, Nivo Charts, Formik, Yup, FullCalendar, and Data Grid to build this entire application. This application consists of Light and Dark Mode, 4 different Charts, 3 different Data Table Pages, FAQ Page, Form Page, and Calendar Integration.',
+    view: '',
+    linkURL: 'https://github.com/TomChaseParis/React--Admin-Dashboard',
+  },
+]
+
+export default ProjectCardData
